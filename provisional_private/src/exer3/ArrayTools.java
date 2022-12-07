08b721a9003dc59e5ad2d30b0fea5b56d96e7c9e
JiaXiaoShuai <2257765565@qq.com>
2022-12-07 16:23:53 +0800

    接口+抽象类+内部类练习

--- provisional_private/src/exer3/ArrayTools.java
+++ provisional_private/src/exer3/ArrayTools.java
@@ -4,6 +4,8 @@ public class ArrayTools {
     public static void print(Object[] arr){
         for(Object obj : arr){
             System.out.print(obj);
+            System.out.println("");
         }
+        System.out.println("");
     }
 }
--- provisional_private/src/exer3/EmployeeService.java
+++ provisional_private/src/exer3/EmployeeService.java
@@ -1,14 +1,39 @@
 package exer3;
 
+import java.util.function.Predicate;
+
 public class EmployeeService {
     private Employee[] arr = new Employee[5];
     private int total;//记录员工对象个数
     public void add(Employee emp){
-        for (int i = 0; i < arr.length; i++) {
+        /*for (int i = 0; i < arr.length; i++) {
             if(arr[i] != null){
                 arr[i] = emp;
                 return;
+            }*/
+
+
+        if(total >= arr.length){
+            System.out.println("数组已满");
+        }
+        arr[total++] = emp;
+    }
+
+    public Employee[] get(Predicate p){
+        int count = 0;
+        for(Employee i:arr){
+            if(p.test(i)){
+                count++;
             }
         }
+        Employee[] result = new Employee[count];
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(p.test(arr[i])){
+                result[k++] = arr[i];
+            }
+        }
+
+        return result;
     }
 }
