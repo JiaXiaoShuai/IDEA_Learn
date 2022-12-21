02ede742addf1ea6ed3b80994b5f6bc1541eb5e7
JiaXiaoshuai <2257765565@qq.com>
2022-12-21 11:10:47 +0800

    exception

---
+++ day0209_15teacher_code/src/exception/TestExceptionType.java
@@ -0,0 +1,40 @@
+package exception;
+
+import java.io.FileNotFoundException;
+
+/*
+1.异常类型的根：java.langThrowable
+Throwable作为类的根仍然Object
+
+Throwable 类是Java语言中所有错误或异常的超类
+只有当对象是此类（或其子类之一）的实例时，才能通过Java虚拟机或者Java throw语句抛出
+类似地，只有此类或其子类之一才可以是catch子句中的参数类型。
+
+2.Throwable有两个大子类
+Error：错误，用于只是合理的应用程序不应该试图捕获的严重问题
+    例如：VirtualMachineError(虚拟机错误)
+    OutOfMemoryError(堆内存溢出)，StackOverflowError（堆内存溢出）
+    这种错误，要么进行硬件升级，要么进行JVM优化，要么进行系统优化
+Exception：异常，指出了合理的应用程序想要捕获的条件
+
+3.Exception：又可以分为两大类
+运行时异常：RuntimeException及其子类，非受检异常。
+编译时异常：除了运行时异常，剩下的都是编译时异常
+
+如何区分是编译时异常类型还是运行时异常类型
+1.看类的继承关系图
+2.看编译器的态度
+编译器会提示你某个异常会发生，那么就是编译时异常
+编译器压根不提示，无论是否发生，那不提示，就是运行时异常
+
+ */
+public class TestExceptionType {
+    public static void main(String[] args) {
+//        FileNotFoundException f = new FileNotFoundException("找不到文件");
+//        throw f;
+        ArrayIndexOutOfBoundsException a = new ArrayIndexOutOfBoundsException();
+        throw a;
+
+//        System.out.println("jajaja");
+    }
+}
---
+++ day0209_15teacher_code/src/exception/TestTryCatch.java
@@ -0,0 +1,42 @@
+package exception;
+/*
+1.语法格式
+try{
+    可能发生异常的代码
+}catch（异常的类型  异常对象名称）{
+    处理异常的代码
+    处理方式：（1）输出打印异常的信息（2）进行相应的逻辑处理
+}catch（异常类型  异常对象名称）{
+
+}
+
+2.注意
+（1）多个catch，异常的类型如果是有父子类关系的话，一定是子类在上，父类在下
+（2）catch（）中一定是异常的类型，不能是其他的类型
+只有是Throwable及其子类类型才能作为catch（）的参数类型
+（3）多个catch如果处理方式都一样，那么在JDK1.7之后，允许这么写
+
+3.执行的特点
+（1）如果try的{}中没有发生异常，那么一个catch都不会去匹配，相当于catch没有用上，直接执行try。。。catch结构下面的代码即可
+（2）如果try的{}中发生了异常，try中剩下的代码就不能在执行了，在发生异常的位置会产生一个对象，并抛出
+    A：catch分支会从上往下一次去检查，依次去尝试捕获这个异常对象，如果可以捕获到，那么就会继续执行try。。。catch结构下面的代码
+    B：所有catch分支都无法匹配，如果不是main方法，就会把异常往上抛，如果是main，程序就挂了
+ */
+public class TestTryCatch {
+    public static void main(String[] args) {
+        try{
+            int a= 1;
+            int b = 0;
+            System.out.println("a = "+a);
+            System.out.println("b = "+b);
+            int result = a/b;//发生异常，try中剩下的代码就不再执行了 ArithmeticException
+            System.out.println("result = "+result);
+        }catch(ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }catch (ArithmeticException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("后续代码");
+    }
+}
