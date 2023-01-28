aefc6d174ab80b87b1c69a5fa6256f031abd0444
JiaXiaoShuai <2257765565@qq.com>
2023-01-28 17:34:06 +0800

    doc comments（文档注释）

---
+++ provisional_private/src/linshi/Test.java
@@ -0,0 +1,20 @@
+package linshi;
+
+/**
+ * for的三种死循环
+ */
+public class Test {
+    public static void main(String[] args) {
+        /*for (; ; ) {
+            System.out.println("我爱你！");
+        }*/
+
+        /*for (int i = 0; i < 10; ) {
+            System.out.println("woaini");
+        }*/
+
+        for(;true;){
+            System.out.println("woai");
+        }
+    }
+}
