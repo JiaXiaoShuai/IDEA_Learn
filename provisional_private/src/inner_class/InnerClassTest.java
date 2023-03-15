f3dd215b606f0acfccd3a04d624177cb937b54f4
JiaXiaoShuai <2257765565@qq.com>
2023-03-15 10:45:12 +0800

    非静态内部类继承

---
+++ provisional_private/src/inner_class/InnerClassTest.java
@@ -0,0 +1,18 @@
+package inner_class;
+
+public class InnerClassTest extends InnerClass.K {
+    public InnerClassTest(InnerClass innerClass){
+        innerClass.super();
+    }
+
+    @Override
+    public void method() {
+        super.method();
+    }
+
+    public static void main(String[] args) {
+        InnerClass innerClass = new InnerClass();
+        InnerClassTest in = new InnerClassTest(innerClass);
+        in.method();
+    }
+}
