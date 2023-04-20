ea3c2ee8b40d6638025fe20f12578163ca27f95e
JiaXiaoShuai <2257765565@qq.com>
2023-04-20 17:26:26 +0800

    集合

---
+++ day0214_18teacher_code/src/com/jc/coll/TestCollection.java
@@ -0,0 +1,16 @@
+package com.jc.coll;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TestCollection {
+    @Test
+    public void test01(){
+        String[] arr = {"1","2","3"};
+        System.out.println(arr.toString());
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
