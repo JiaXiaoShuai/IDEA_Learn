bdd0bb411194826f9800d3d4dfa97db50c85f6cb
JiaXiaoShuai <2257765565@qq.com>
2023-07-17 17:25:58 +0800

    collection集合-work

--- day0217_21teacher_code/src/com/jc/collection/Man.java
+++ day0217_21teacher_code/src/com/jc/collection/Man.java
@@ -5,4 +5,12 @@ public class Man extends Person{
     public Man(String name, int age) {
         super(name, age);
     }
+
+    public static void main(String[] args) {
+        int i = 15&1;
+        int j = 15&2;
+        System.out.println(i);
+        System.out.println(j);
+
+    }
 }
